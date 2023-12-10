33f530e529117952889b6efb054fdc211290a011
geravvene <romanov17903@gmail.com>
2023-12-10 16:12:14 +0400

    fix

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-import express from "express";
-import { MongoClient } from "mongodb";
-import { ObjectId } from "bson";
-import { getController } from "./controllers/getController.js";
-import { putController } from "./controllers/putController.js";
-import { postController } from "./controllers/postController.js";
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import { ObjectId } from 'bson';
+import { getController } from './controllers/getController.js';
+import { putController } from './controllers/putController.js';
+import { postController } from './controllers/postController.js';
 
-const client = new MongoClient('mongodb+srv://geravvene:Mhcv3lAti7HZWEnd@wordigma.rmxf6nd.mongodb.net/?retryWrites=true');
+const client = new MongoClient(
+  'mongodb+srv://geravvene:Mhcv3lAti7HZWEnd@wordigma.rmxf6nd.mongodb.net/?retryWrites=true'
+);
 
-const db = client.db("Wordigma");
+const db = client.db('Wordigma');
 
 const app = express();
 
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Access-Control-Allow-Headers"
+    'Access-Control-Allow-Origin',
+    'https://geravvene.github.io/Wordigma'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers'
   );
   next();
 });
@@ -36,9 +41,9 @@ postController(app, db, ObjectId);
 
 app.listen(5172);
 
-process.on("SIGINT", async () => {
+process.on('SIGINT', async () => {
   await client.close();
-  console.log("Приложение завершило работу");
+  console.log('Приложение завершило работу');
   process.exit();
 });
 

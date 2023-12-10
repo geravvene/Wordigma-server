2182243528b28ce861edd875b845f4c14ddf394f
geravvene <romanov17903@gmail.com>
2023-12-10 15:56:52 +0400

    no

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/Wordigma");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
